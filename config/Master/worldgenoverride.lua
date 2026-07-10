d22b2fe20a02594af233ded64c2d42c6a29bccab
cetaceang <cetaceang@users.noreply.github.com>
2026-07-10 20:04:00 +0800

    Allow resurrection at the Florid Postern

--- config/Master/worldgenoverride.lua
+++ config/Master/worldgenoverride.lua
@@ -5,5 +5,8 @@ return {
     -- Disable maximum-health loss after resurrection. This master-controlled
     -- world setting is synchronized to the Caves shard automatically.
     healthpenalty = "none",
+    -- Klei's option name intentionally uses the historical "resurection"
+    -- spelling. Allow ghosts to resurrect at the Florid Postern at any time.
+    portalresurection = "always",
   },
 }
